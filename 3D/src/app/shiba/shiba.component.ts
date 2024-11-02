15d4cd865ab09c5b5b5432186e02124fee031efa
Tiago1191583 <1191583@isep.ipp.pt>
2024-11-02 12:02:23 +0000

    US 6.5.1 updated shiba component #45

--- 3D/src/app/shiba/shiba.component.ts
+++ 3D/src/app/shiba/shiba.component.ts
@@ -1,12 +1,24 @@
-import { Component } from '@angular/core';
+import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js';
+import * as THREE from 'three';
 
-@Component({
-  selector: 'app-shiba',
-  standalone: true,
-  imports: [],
-  templateUrl: './shiba.component.html',
-  styleUrl: './shiba.component.scss'
-})
-export class ShibaComponent {
 
+export class ShibaComponent extends THREE.Group {
+
+  constructor() {
+    super();
+
+    const gltfLoader = new GLTFLoader();
+    gltfLoader.load('./assets/shiba/scene.gltf', (gltfScene) => {
+      gltfScene.scene.scale.set(2, 2, 2);
+      gltfScene.scene.position.set(-15, 13.8, -19);
+      gltfScene.scene.rotation.y = Math.PI / 6; 
+      gltfScene.scene.traverse((child) => {
+        if (child instanceof THREE.Mesh) {
+          child.castShadow = true;     
+          child.receiveShadow = true;  
+        }
+      });
+      this.add(gltfScene.scene);
+    });
+  }
 }
