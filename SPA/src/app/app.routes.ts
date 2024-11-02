a3103935ea143ccf2cdb0c6774ef6813943108f3
1201313_AnaCosta <1201313@isep.ipp.pt>
2024-11-02 14:26:33 +0000

    US 6.2.13 - Staff component. Redirecting homepage to Staff page. #36

--- SPA/src/app/app.routes.ts
+++ SPA/src/app/app.routes.ts
@@ -1,9 +1,20 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
+import { StaffComponent } from './staff/staff.component';
 
 export const routes: Routes = [
   {
     path: '',
+    component: HomeComponent,
+    title: 'Home page',
+  },
+  {
+    path: 'staff',
+    component: StaffComponent,
+    title: 'Staff page',
+  },
+  {
+    path: '**',
     component: HomeComponent,
     title: 'Home page',
   },
