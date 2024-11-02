3cea55a5161f5a6565aa15b90c515c7a1eb15cea
Tiago1191583 <1191583@isep.ipp.pt>
2024-11-02 11:59:31 +0000

    US 6.5.1 updated door component #45

--- 3D/src/app/door/door.component.ts
+++ 3D/src/app/door/door.component.ts
@@ -1,12 +1,23 @@
-import { Component } from '@angular/core';
+import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js';
+import * as THREE from 'three';
 
-@Component({
-  selector: 'app-door',
-  standalone: true,
-  imports: [],
-  templateUrl: './door.component.html',
-  styleUrl: './door.component.scss'
-})
-export class DoorComponent {
 
+export class DoorComponent extends THREE.Group {
+
+  constructor() {
+    super();
+
+    const gltfLoader = new GLTFLoader();
+    gltfLoader.load('./assets/door/scene.gltf', (gltfScene) => {
+      gltfScene.scene.scale.setScalar(0.02); 
+      gltfScene.scene.position.set(20, 0, 25);
+      gltfScene.scene.traverse((child) => {
+        if (child instanceof THREE.Mesh) {
+          child.castShadow = true;     
+          child.receiveShadow = true;
+        }
+      });
+      this.add(gltfScene.scene);
+    });
+  }
 }
