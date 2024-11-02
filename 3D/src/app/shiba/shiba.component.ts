273de05dcc8b94a48727d48c2914e6a7a3e020d0
Tiago1191583 <1191583@isep.ipp.pt>
2024-11-02 10:47:41 +0000

    US 6.5.1 new component shiba #45

---
+++ 3D/src/app/shiba/shiba.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-shiba',
+  standalone: true,
+  imports: [],
+  templateUrl: './shiba.component.html',
+  styleUrl: './shiba.component.scss'
+})
+export class ShibaComponent {
+
+}
