9d24534029c914e40acd0e227ac0284719b4a3df
Tiago1191583 <1191583@isep.ipp.pt>
2024-11-02 12:04:47 +0000

    US 6.5.1 updated interior wall component #45

--- 3D/src/app/interior-wall/interior-wall.component.ts
+++ 3D/src/app/interior-wall/interior-wall.component.ts
@@ -1,12 +1,22 @@
-import { Component } from '@angular/core';
+import * as THREE from "three";
+import WallComponent from "../wall/wall.component";
 
-@Component({
-  selector: 'app-interior-wall',
-  standalone: true,
-  imports: [],
-  templateUrl: './interior-wall.component.html',
-  styleUrl: './interior-wall.component.scss'
-})
-export class InteriorWallComponent {
+export default class InteriorWallComponent extends THREE.Group {
+  constructor() {
+    super();
 
+    var wall = new WallComponent("./assets/textures/wall1.png", "./assets/textures/whitefloor.png");
+    
+    this.add(wall);
+
+
+
+    var woodPanel = new WallComponent("./assets/textures/wood.png", "./assets/textures/wood.png");
+    woodPanel.scale.set(1.001, 0.1, 1);
+    woodPanel.rotation.y = -Math.PI;  // Rotate 90 degrees to lay flat
+
+    woodPanel.position.set(0,-0.11,0.01);
+    this.add(woodPanel);
+
+  }
 }
