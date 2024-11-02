97db071d2303b043435723878296c856d17fc94b
Tiago1191583 <1191583@isep.ipp.pt>
2024-11-02 10:46:14 +0000

    US 6.5.1 new component instruments #45

---
+++ 3D/src/app/instruments/instruments.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-instruments',
+  standalone: true,
+  imports: [],
+  templateUrl: './instruments.component.html',
+  styleUrl: './instruments.component.scss'
+})
+export class InstrumentsComponent {
+
+}
