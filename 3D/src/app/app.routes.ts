46f4f74a60f39aea9336e26f11160c6f0f2ac3de
Tiago1191583 <1191583@isep.ipp.pt>
2024-11-02 12:07:24 +0000

    US 6.5.1 updated route with path to hospital component #45

--- 3D/src/app/app.routes.ts
+++ 3D/src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { HospitalComponent } from './hospital/hospital.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+
+    { path: '', redirectTo: '/hospital', pathMatch: 'full'},
+    { path: 'hospital', component: HospitalComponent},
+
+];
