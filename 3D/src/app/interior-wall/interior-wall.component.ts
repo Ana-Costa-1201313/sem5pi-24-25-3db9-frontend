1b18a45cc2ef5242e296b42563d5a211b2c5c499
Tiago1191583 <1191583@isep.ipp.pt>
2024-11-02 10:47:23 +0000

    US 6.5.1 new component interior wall #45

---
+++ 3D/src/app/interior-wall/interior-wall.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-interior-wall',
+  standalone: true,
+  imports: [],
+  templateUrl: './interior-wall.component.html',
+  styleUrl: './interior-wall.component.scss'
+})
+export class InteriorWallComponent {
+
+}
