96f9f48bca1342cce4e183942dabac1af8bdc959
Tiago1191583 <1191583@isep.ipp.pt>
2024-11-07 23:00:38 +0000

    US 6.2.21 initial operation type model #44

---
+++ SPA/src/app/model/operationType.ts
@@ -0,0 +1,9 @@
+
+export type OperationType = {
+    id: string | null |undefined;
+    name: string | null | undefined;
+    preparation: number | null | undefined;
+    surgery: number | null | undefined;
+    cleaning: number | null | undefined;
+    active: boolean | null | undefined;
+}
