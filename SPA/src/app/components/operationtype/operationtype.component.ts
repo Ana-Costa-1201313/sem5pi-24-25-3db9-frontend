0036a88c04a01ba5d24c0506d2d2812d41af3afa
Tiago1191583 <1191583@isep.ipp.pt>
2024-11-08 14:09:45 +0000

    US 6.2.21 added modal to view details #44

--- SPA/src/app/components/operationtype/operationtype.component.ts
+++ SPA/src/app/components/operationtype/operationtype.component.ts
@@ -2,26 +2,34 @@ import { CommonModule } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
 import { TableModule } from 'primeng/table';
 import { OperationType } from '../../model/operationType.model';
-import { OperationTypeService } from '../../services/operationType.service'; 
+import { OperationTypeService } from '../../services/operationType.service';
+import { DialogModule } from 'primeng/dialog';
 
 @Component({
   selector: 'app-operationtype',
   standalone: true,
-  imports: [CommonModule, TableModule],
+  imports: [CommonModule, TableModule, DialogModule],
   templateUrl: './operationtype.component.html',
   styleUrl: './operationtype.component.css'
 })
 export class OperationtypeComponent implements OnInit {
 
   operationTypeList: OperationType[] = [];
+  currentOpType: OperationType | null = null;
+  showDetails: boolean = false;
 
-  constructor(private service: OperationTypeService){
+  constructor(private service: OperationTypeService) {
   }
 
   ngOnInit(): void {
     this.service.getOperationTypeList().subscribe((op) => {
       this.operationTypeList = op;
     })
+  }
+
+  openDetailsModal(opType: OperationType): void{
+    this.currentOpType = opType;
+    this.showDetails = true;
   }
 
 }
