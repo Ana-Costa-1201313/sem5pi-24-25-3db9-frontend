9d1353c78de0e43df800544e25e92244bf0cd410
Luis Reis <1210998@isep.ipp.pt>
2024-11-11 19:55:51 +0000

    Some updates Button to show details with 3 categories: General Information, Contact Information and Medical Information #32

--- SPA/src/app/interceptors/interceptor.ts
+++ SPA/src/app/interceptors/interceptor.ts
@@ -3,7 +3,7 @@ import { HttpHandlerFn, HttpInterceptorFn, HttpRequest } from "@angular/common/h
 export const authenticationInterceptor: HttpInterceptorFn = (req: HttpRequest<unknown>, next:
     HttpHandlerFn) => {
     const modifiedReq = req.clone({
-        headers: req.headers.set('Authorization', `Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJuYW1laWQiOiJhZG1pbkBob3RtYWlsLmNvbSIsInJvbGUiOiJBZG1pbiIsImp0aSI6IjY1OGY1MTQwLWNmMTQtNDdlOC04NzBjLWJiNjNmNTk2ZjhiYSIsImlhdCI6MTczMTA4OTA2NiwibmJmIjoxNzMxMDg5MDY2LCJleHAiOjE3MzEzNDgyNjYsImlzcyI6IkF1dGhfQmFja09mZmljIiwiYXVkIjoiQVVUSCJ9.X725a9ka9S2N4JjmC9oH4Qr-WXKj2dJrYtyVWlSzCtc`),
+        headers: req.headers.set('Authorization', `Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJuYW1laWQiOiJhZG1pbkBob3RtYWlsLmNvbSIsInJvbGUiOiJBZG1pbiIsImp0aSI6IjVjY2NiNTY2LTc1YzEtNDlkYy1iZWY1LTE0ODhmNTAwY2ExZCIsImlhdCI6MTczMTM1MjU4OSwibmJmIjoxNzMxMzUyNTg5LCJleHAiOjE3MzE2MTE3ODksImlzcyI6IkF1dGhfQmFja09mZmljIiwiYXVkIjoiQVVUSCJ9.D3sJOiI-q7EhmHKzF0FScaIkZrmTzdY4cUxqsdT8D0Y`),
     });
 
     return next(modifiedReq);
