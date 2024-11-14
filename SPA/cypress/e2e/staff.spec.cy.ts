b8134515d7c8aa833e884963ed2dd2f4c8e62497
1201313_AnaCosta <1201313@isep.ipp.pt>
2024-11-14 22:56:22 +0000

    US 6.2.11 - E2e tests. #34

--- SPA/cypress/e2e/staff.spec.cy.ts
+++ SPA/cypress/e2e/staff.spec.cy.ts
@@ -10,29 +10,82 @@ describe('Staff Page Test', () => {
 });
 
 describe('Staff Test', () => {
+  it('Create staff', () => {
+    cy.visit('/staff');
+
+    cy.get('#buttonCreate').click();
+
+    cy.get('#name').type('TestName');
+    cy.get('#licenseNumber').type('17825');
+    cy.get('#phone').type('992948964');
+    cy.get('#recruitmentYear').type('2022');
+
+    cy.get('#role').click();
+    cy.get('.p-dropdown-item').contains('NURSE').click();
+
+    cy.get('#buttonSubmit').find('button').click({ force: true });
+
+    cy.get('#message').should(($div) => {
+      const text = $div.text();
+
+      expect('Success!Your Staff Profile was added with success').equal(text);
+    });
+  });
+
+  it('Edit staff', () => {
+    cy.visit('/staff');
+
+    cy.get('input[aria-label="Filter Name"]').type('TestName{enter}');
+
+    cy.get('.p-datatable-striped tbody')
+      .contains('tr', 'TestName')
+      .within(() => {
+        cy.get('#buttonEdit').click();
+      });
+    cy.get('#phone').type('992945969');
+
+    cy.get('#buttonSubmit').find('button').click({ force: true });
+
+    cy.get('#message').should(($div) => {
+      const text = $div.text();
+
+      expect('Success!Your Staff Profile was edited with success').equal(text);
+    });
+  });
+
   it('Get staff details', () => {
     cy.visit('/staff');
 
-    cy.get('#buttonDetails').click();
+    cy.get('input[aria-label="Filter Name"]').type('TestName{enter}');
+
+    cy.get('.p-datatable-striped tbody')
+      .contains('tr', 'TestName')
+      .within(() => {
+        cy.get('#buttonDetails').click();
+      });
+
+      cy.get('#licenseNumber').should('contain.text', '17825');
+    });
+
+  it('Deactivate staff', () => {
+    cy.visit('/staff');
+
+    cy.get('input[aria-label="Filter Name"]').type('TestName{enter}');
+
+    cy.get('.p-datatable-striped tbody')
+      .contains('tr', 'TestName')
+      .within(() => {
+        cy.get('#buttonDeactivate').click();
+      });
+
+    cy.get('#buttonConfirmDelete').click();
+
+    cy.get('#message').should(($div) => {
+      const text = $div.text();
+
+      expect('Success!Your Staff Profile was deactivated with success').equal(
+        text
+      );
+    });
   });
-
-  // it('Create staff', () => {
-  //   cy.visit('/staff');
-
-  //   cy.get('#buttonCreate').click();
-
-  //   cy.get('#name').type('name');
-  //   cy.get('#licenseNumber').type('123');
-  //   cy.get('#phone').type('999999999');
-  //   cy.get('#specialization').type('specialization');
-  //   cy.get('#availabilitySlots').type('availabilitySlots');
-  //   cy.get('#role').type('Nurse');
-  //   cy.get('#recruitmentYear').type('2022');
-
-  //   cy.get('#buttonSubmit').click({force: true});
-  //   cy.get('#success').should(($div) => {
-  //     const text = $div.text();
-  //     expect('Your Staff Profile was added with success').equal(text);
-  //   });
-  //});
 });
