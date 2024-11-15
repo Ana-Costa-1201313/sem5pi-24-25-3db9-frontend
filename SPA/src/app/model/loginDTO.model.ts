97785c9d9fcaea499be0d7822cd042ce99edefbc
1221695 <1221695@isep.ipp.pt>
2024-11-15 17:47:17 +0000

    US6.2.4 - logindto

---
+++ SPA/src/app/model/loginDTO.model.ts
@@ -0,0 +1,9 @@
+export type LoginDTO = {
+  username: string | null;
+  password: string | null;
+  googleCredentials: string | null;
+  jwt: string | null;
+  role: string | null;
+  tokenCreated: Date | null;
+  a: number | null;
+}
