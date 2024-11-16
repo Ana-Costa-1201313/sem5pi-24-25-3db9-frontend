bc68b34659e7952053b5a8a7664715388181ae46
1221695 <1221695@isep.ipp.pt>
2024-11-16 14:59:22 +0000

    US6.2.4 -   1.  logica para guardar jwt token em cookie http only não acessivel por jaavascript   2.  interceptor com logica para incluir jwt token no header de pedidos que saem

--- SPA/src/app/interceptors/interceptor.ts
+++ SPA/src/app/interceptors/interceptor.ts
@@ -1,15 +1,15 @@
 import { HttpHandlerFn, HttpInterceptorFn, HttpRequest } from "@angular/common/http";
 
+//export const authenticationInterceptor: HttpInterceptorFn = (req: HttpRequest<unknown>, next:
+//    HttpHandlerFn) => {
+//    const modifiedReq = req.clone({
+//        headers: req.headers.set('Authorization', `Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJuYW1laWQiOiJhZG1pbkBob3RtYWlsLmNvbSIsInJvbGUiOiJBZG1pbiIsImp0aSI6ImQ3MWUwMGFlLTcwNjYtNDEzNi04M2E1LTIwMzRmNTk1ZGRkYyIsImlhdCI6MTczMDU3MTY5MSwibmJmIjoxNzMwNTcxNjkxLCJleHAiOjE3MzA4MzA4OTEsImlzcyI6IkF1dGhfQmFja09mZmljIiwiYXVkIjoiQVVUSCJ9.YFGc-vCCLvzT12mvE7Xa7UkZlpWN1-i2u_sTgJl5KbQ`),
+//    });
+
+//    return next(modifiedReq);
+//};
+
 export const authenticationInterceptor: HttpInterceptorFn = (req: HttpRequest<unknown>, next:
-    HttpHandlerFn) => {
-    const modifiedReq = req.clone({
-        headers: req.headers.set('Authorization', `Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJuYW1laWQiOiJhZG1pbkBob3RtYWlsLmNvbSIsInJvbGUiOiJBZG1pbiIsImp0aSI6ImQ3MWUwMGFlLTcwNjYtNDEzNi04M2E1LTIwMzRmNTk1ZGRkYyIsImlhdCI6MTczMDU3MTY5MSwibmJmIjoxNzMwNTcxNjkxLCJleHAiOjE3MzA4MzA4OTEsImlzcyI6IkF1dGhfQmFja09mZmljIiwiYXVkIjoiQVVUSCJ9.YFGc-vCCLvzT12mvE7Xa7UkZlpWN1-i2u_sTgJl5KbQ`),
-    });
-
-    return next(modifiedReq);
-};
-
-export const authenticationInterceptor2: HttpInterceptorFn = (req: HttpRequest<unknown>, next:
   HttpHandlerFn) => {
 
   // Function to get the JWT token from cookies
