cf9b6e1b7d4578bfe05a260cf538570329d819cc
Tiago1191583 <1191583@isep.ipp.pt>
2024-11-18 23:03:53 +0000

    US 6.1.2 menu according to the role #50

--- SPA/src/app/components/home/home.component.ts
+++ SPA/src/app/components/home/home.component.ts
@@ -1,14 +1,40 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { CardModule } from 'primeng/card';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [CardModule, RouterModule],
+  imports: [CardModule, RouterModule, CommonModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
+  role: string | null = null;
+
+  constructor() {
+
+  }
+
+  ngOnInit(): void {
+
+    const sessionData = {
+      username: "Username",
+      loggedIn: true,
+      role: "Admin",
+    };
+
+    sessionStorage.setItem('SessionUtilizadorInfo', JSON.stringify(sessionData));
+    const session = this.getSession();
+    this.role = session?.role || null; // Extract role
+
+  }
+
+  private getSession() {
+    const session = sessionStorage.getItem('SessionUtilizadorInfo');
+    return session ? JSON.parse(session) : null;
+  }
+
 
 }
