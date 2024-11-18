cf4191b8eb0cc942147b16523c5f8987b79a9ca7
Tiago1191583 <1191583@isep.ipp.pt>
2024-11-18 23:55:06 +0000

    US 6.1.2 menubar unfinished #50

--- SPA/src/app/components/home/home.component.ts
+++ SPA/src/app/components/home/home.component.ts
@@ -1,20 +1,24 @@
 import { CommonModule } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { MenuItem } from 'primeng/api';
 import { CardModule } from 'primeng/card';
+import { MenubarModule } from 'primeng/menubar';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [CardModule, RouterModule, CommonModule],
+  imports: [CardModule, RouterModule, CommonModule, MenubarModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
 export class HomeComponent implements OnInit {
   role: string | null = null;
 
-  constructor() {
+  items: MenuItem[] | undefined;
+  endItems: MenuItem[] | undefined;
 
+  constructor() {
   }
 
   ngOnInit(): void {
@@ -27,7 +31,65 @@ export class HomeComponent implements OnInit {
 
     sessionStorage.setItem('SessionUtilizadorInfo', JSON.stringify(sessionData));
     const session = this.getSession();
-    this.role = session?.role || null; // Extract role
+    this.role = session?.role || null;
+
+    this.items = [
+      {
+        label: 'Home',
+        icon: 'pi pi-home',
+        routerLink: ''
+      },
+      {
+        label: 'SubMenus',
+        icon: 'pi pi-bars',
+        items: [
+          {
+            label: 'User',
+            icon: 'pi pi-angle-right',
+            routerLink: '',
+            visible: this.role === 'Admin'
+          },
+          {
+            label: 'Patient',
+            icon: 'pi pi-angle-right',
+            routerLink: '',
+            visible: this.role === 'Admin'
+          },
+          {
+            label: 'Staff',
+            icon: 'pi pi-angle-right',
+            routerLink: 'staff',
+            visible: this.role === 'Admin'
+          },
+          {
+            label: 'Specialization',
+            icon: 'pi pi-angle-right',
+            routerLink: 'specialization',
+            visible: this.role === 'Admin'
+          },
+          {
+            label: 'Operation Type',
+            icon: 'pi pi-angle-right',
+            routerLink: 'operationtype',
+            visible: this.role === 'Admin'
+          },
+          {
+            label: 'Operation Request',
+            icon: 'pi pi-angle-right',
+            routerLink: '',
+            visible: this.role === 'Admin' || this.role === 'Doctor'
+          },
+        ].filter(item => item.visible)
+      }
+    ];
+
+    this.endItems = [
+      {
+        label: 'LogOut',
+        icon: 'pi pi-sign-out',
+        routerLink: ''
+      }
+    ];
 
   }
 
