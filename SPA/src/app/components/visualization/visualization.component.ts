f6f103b29a913327f4ef81f4762d78cd17bb13bd
Tiago1191583 <1191583@isep.ipp.pt>
2024-11-22 10:30:25 +0000

    US 6.1.1 updated form with date and redirected to hospital 3d representation #49 #51

--- SPA/src/app/components/visualization/visualization.component.ts
+++ SPA/src/app/components/visualization/visualization.component.ts
@@ -1,10 +1,39 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule, ReactiveFormsModule, FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { ButtonModule } from 'primeng/button';
+import { CalendarModule } from 'primeng/calendar';
+import { TableModule } from 'primeng/table';
 
 @Component({
   selector: 'app-visualization',
+  standalone: true,
+  imports: [
+    CommonModule,
+    TableModule,
+    FormsModule,
+    ButtonModule,
+    ReactiveFormsModule,
+    CalendarModule,
+  ],
   templateUrl: './visualization.component.html',
-  styleUrl: './visualization.component.css'
+  styleUrls: ['./visualization.component.css']
 })
 export class VisualizationComponent {
+  formGroup: FormGroup;
 
+  constructor(private fb: FormBuilder) {
+    this.formGroup = this.fb.group({
+      date: [null, Validators.required],
+    });
+  }
+
+  submitForm() {
+    if (this.formGroup.valid) {
+      const selectedDate = this.formGroup.get('date')?.value;
+      console.log('Selected Date:', selectedDate);
+
+      window.location.href="http://localhost:4201/hospital";
+    }
+  }
 }
